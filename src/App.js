011f0cd46ef06b334cc78182778e9563b797d8b8
Tanaka930 <55372083+Tanaka930@users.noreply.github.com>
2021-08-06 18:44:11 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { CssModuels } from "./compornents/CssModules";
+import { InlineStyle } from "./compornents/inlineStyle";
+import { StyledJsx } from "./compornents/StyledJsx";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <InlineStyle />
+      <CssModuels />
+      <StyledJsx />
     </div>
   );
 }
---
+++ src/compornents/CssModules.jsx
@@ -0,0 +1,10 @@
+import classes from "./CssModule.scss";
+
+export const CssModuels = () => {
+  return (
+    <div className={classes.container}>
+      <p>- CSS Moduels -</p>
+      <button>FIGHT!!</button>
+    </div>
+  );
+};
